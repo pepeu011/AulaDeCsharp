94afa90662ba1837efde2903c4f193c79b7479df
Pedro <pc90026@gmail.com>
2024-07-31 11:42:05 -0300

    Funções

---
+++ TiposDeDadosInteiros/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TiposDeDadosInteiros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de Dados Inteiros
+            sbyte valor_sbyte = 127;
+            byte valor_byte = 255;
+            short valor_short = 32767;
+            ushort valor_ushort = 65535;
+            int valor_int = 2147483647;
+            uint valor_uint = 4294967295;
+            long valor_long = 9223372036854775807;
+            ulong valor_ulong = 18446744073709551615;
+            nint valor_nint = 2147483647;
+            nuint valor_nuint = 4294967295;
+        }
+    }
+}
