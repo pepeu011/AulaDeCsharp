8f14a527b00edfd53d31cef4b1a6e3c5f3f47696
Pedro <pc90026@gmail.com>
2024-07-31 11:44:02 -0300

    funções

---
+++ TiposdeDadosPorReferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TiposdeDadosPorReferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Tipo de dados por referência 
+           string texto = "Essa é uma mensagem";
+           Console.WriteLine(texto);
+
+            //Dados object
+            object valor_inteiro = 123;
+            object valor_real = 123.321;
+            object valor_texto = "Esse é um texto";
+            object valor_caracter = 'V';
+            object valor_logico = true;
+            
+        }
+    }
+}
