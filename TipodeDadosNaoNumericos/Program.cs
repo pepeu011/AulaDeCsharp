beb4c7616a2beb6fd4a31fe101883d1d0cb829be
Pedro <pc90026@gmail.com>
2024-07-31 11:37:42 -0300

    Funções

---
+++ TipodeDadosNaoNumericos/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TipodeDadosNaoNumericos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Valores Lógicos
+
+            bool verdadeiro = true;
+            bool falso = false;
+
+            // Valores Caracteres 
+            char valor1 = 'A';
+            char valor2 = '\u0041';
+
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+        }
+    }
+}
