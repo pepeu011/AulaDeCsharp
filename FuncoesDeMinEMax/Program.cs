bb62fb62f1a6415881f73faf22e45291de116223
Pedro <pc90026@gmail.com>
2024-07-31 11:36:18 -0300

    Funções

---
+++ FuncoesDeMinEMax/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FuncoesDeMinEMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor_int_min = int.MinValue;
+            int valor_int_max = int.MaxValue;
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            decimal valor_decimal_min = decimal.MinValue;
+            decimal valor_decimal_max = decimal.MaxValue;
+            Console.WriteLine(valor_decimal_min);
+            Console.WriteLine(valor_decimal_max);
+        }
+    }
+}
